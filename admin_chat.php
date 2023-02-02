11b6a551a16241e5bdee705c7fa50d2449c710ef
Emir Ay <106332223+emirayemr@users.noreply.github.com>
2023-02-02 13:56:02 +0300

    Add files via upload

---
+++ admin_chat.php
@@ -0,0 +1,67 @@
+<?php
+
+    $URL = "https://turkeylocalelections-step5-default-rtdb.europe-west1.firebasedatabase.app/messages.json";
+
+    function get_messages() { 
+        global $URL;
+        $ch = curl_init();
+        curl_setopt_array($ch, [ CURLOPT_URL => $URL,
+                                CURLOPT_POST => FALSE, // It will be a get request 
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_SSL_VERIFYPEER => false, ]);
+        $response = curl_exec($ch); 
+        curl_close($ch);
+        return json_decode($response, true); 
+    }
+    $msg_res_json = get_messages();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<div class="menu">
+<div class="back"><i class="fa fa-chevron-left"></i> <a href="adminpanel.html"> <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></a></div>
+<div class="name">Local Elections Database Support (Admin)</div>
+<div class="last"><?=date("H.i")?></div>
+</div>
+<ol class="chat">
+<?php
+	if($msg_res_json != null)
+	{
+		$keys = array_keys($msg_res_json);
+		if($keys != null)
+		{
+			for ($i = 0; $i < count($keys); $i++){
+				$chat_msg = $msg_res_json[$keys[$i]];
+				$name = $chat_msg['name'];
+				$msg = $chat_msg['msg'];
+				$time = $chat_msg['time'];
+				if ($name == 'admin') {
+					$from = 'self';
+				} else {
+					$from = 'other';
+				}
+			   echo  '
+			   <li class="'.$from.'">
+			   <div class="avatar">
+						<img src="https://i.imgur.com/DY6gND0.png" draggable="false"/>
+					</div>
+						<div class="msg">
+							<p><b>'.$name.'</b></p>
+							<p>'.$msg.'</p>
+							<time>'.$time.'</time>
+						</div>
+					</li>';
+			}
+		}
+	}
+?>
+</ol>
+<form name="form" action = "send_message.php?sender=admin" method="POST">
+    <input name="usermsg" class="textarea" type="text" placeholder="Type here!"/>
+    <input type="submit" style="display: none" />
+</form>
---
+++ alldistricts.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All Districts</title>
+</head>
+
+<link rel="stylesheet" href="projstyle.css">
+
+<body>
+
+<ul>
+    <li><a class="home" href="mainpage.html">Main</a></li>
+    <li><a href="istanbul.php">Istanbul</a></li> 
+    <li><a href="ankara.php">Ankara</a></li>
+    <li><a href="izmir.php">Izmir</a></li>
+    <li><a href="alldistricts.php">All Districts</a></li>
+    <li><a style="background-color:black; color:white;" href="adminentry.html">Admin Panel</a></li>
+</ul>
+<br><br>
+
+<div align="center">
+
+	<table>
+
+<tr> <th> ID </th> <th> NAME </th> <th>NEW MAYOR</th> <th>WINNER PARTY</th> </tr> 
+
+<?php
+
+include "config.php";
+
+$sql_statement = "SELECT * FROM districts";
+
+$result = mysqli_query($db, $sql_statement);
+
+$sql2 = "SELECT * FROM candidates";
+
+$r2 = mysqli_query($db, $sql2);
+
+$sql3 = "SELECT * FROM nom_by";
+
+$r3 = mysqli_query($db, $sql3);
+
+$sql4 = "SELECT * FROM parties";
+
+$r4 = mysqli_query($db, $sql4);
+
+$pid = 1;
+
+while($row = mysqli_fetch_assoc($result))
+{
+  $id = $row['did'];
+  $name = $row['dname'];
+  $voters = $row['dvoters'];
+            mysqli_data_seek($r2,0);
+            while ($w = mysqli_fetch_assoc($r2)) {
+                $cid = $w['cid'];
+                $cname = $w['cname'];
+                if ($cid-$id == 10000) {
+
+                  $pid == 1;
+                    mysqli_data_seek($r3,0);
+                    while ($p = mysqli_fetch_assoc($r3)) {
+                        if ($cid == $p['cid']) {
+                            $pid = $p['pid'];
+                        }       
+                        mysqli_data_seek($r4,0);
+                        while ($p2 = mysqli_fetch_assoc($r4)) {
+                            if ($pid == $p2['pid']) {
+                                $pname = $p2['pname'];
+                            }
+                        }
+                    }
+
+                    echo "<tr>" . "<th>" . $id . "</th>" . "<th>" . $name . "</th>" . "<th>" . $cname . "</th>" . "<th>"  . $pname . "</th>" . "</tr>";
+
+                }
+            }
+
+        }
+
+?>
+
+</table>
+</div>
+
+</body>
+</html>
